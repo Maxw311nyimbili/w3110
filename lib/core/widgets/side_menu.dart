50c6926145cc31e7542ddc825a04b8014a4a206f
Maxw311 <nyimbilimaxwell9@gmail.com>
2026-02-25 21:40:00 +0000

    fix: resolve missing imports and build errors in SideMenu and SplashPage

--- lib/core/widgets/side_menu.dart
+++ lib/core/widgets/side_menu.dart
@@ -5,6 +5,7 @@ import 'package:cap_project/core/theme/app_text_styles.dart';
 import 'package:cap_project/features/auth/cubit/cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:cap_project/core/widgets/brand_logo.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({super.key});
